fa4bb41eee5748957e721f86f3a9e5ecb2d15d38
thasan01 <thasan2004@yahoo.com>
2025-03-29 23:37:17 -0400

    missed a file

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as fp:
+    desc_text = fp.read()
+
 setup(
-    name="viutils",
-    version="0.0.1",
+    name="ivutils",
+    version="0.0.2",
     license="MIT license",
+    long_description=desc_text,
+    long_description_content_type= "text/markdown",
     packages=find_packages(),
-    install_requires=["opencv-python~=4.10.0.84",
-                      "numpy~=2.1.3",
-                      "pillow~=11.0.0"
-                      ]
+    install_requires=[
+        "opencv-python~=4.10.0.84",
+        "numpy~=2.1.3",
+        "pillow~=11.0.0"
+    ]
 )
