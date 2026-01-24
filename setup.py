7ffcf094a8166302da380e3a79f686e882ddfc95
thasan01 <thasan2004@yahoo.com>
2026-01-23 21:16:04 -0500

    fixed dependencies to play nice with Google Colab

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
+import os
 from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
 with open("README.md") as fp:
     desc_text = fp.read()
 
 setup(
     name="ivutils",
-    version="0.0.5",
+    version="0.0.7",
     license="MIT license",
     long_description=desc_text,
     long_description_content_type= "text/markdown",
+    url="https://github.com/thasan01/ivutils",
+    keywords=[
+            "computer-vision", "opencv", "image-processing", "video-processing", 
+            "imaging", "utilities",  "transformation", "automation", "task"
+        ],    
     packages=find_packages(),
-    install_requires=[
-        "opencv-python~=4.10.0.84",
-        "numpy~=2.1.3",
-        "pillow~=11.0.0",
-        "PyYAML~=6.0.3"
-    ],
+    install_requires=parse_requirements("requirements.txt"),
     entry_points={
         'console_scripts': [
             'ivutils = ivutils.cli:run',
